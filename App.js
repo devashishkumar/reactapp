0f775aa1e52dab540f2afecd93f04544b4c08ce5
Ashish <dev.ashishkumar0512@gmail.com>
2023-12-30 11:38:21 +0530

    add build command and create h1 tag in react

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "Hello World 1"), React.createElement("h2", {}, "Hello World 2")])
-);
+const heading = React.createElement("h1", {id: "heading"}, "React App");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
