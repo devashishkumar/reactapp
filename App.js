468aced0de89961424eaa6c8ce0e6682b763e175
Ashish <dev.ashishkumar0512@gmail.com>
2023-12-30 12:01:56 +0530

    react jsx

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1", {id: "heading"}, "React App");
+// JSX (Transpiled Before it reaches the JS) Parcel - Babel
+
+// JSX => Babel Transpiled it to React.createElement
+const headingjsx = (<h1 className="heading" tabIndex="1">
+    React App
+    </h1>)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(headingjsx);
