e42f8f5b15575d71892148d418ca5e82e3f2ddc9
Ashish <dev.ashishkumar0512@gmail.com>
2023-12-30 12:15:01 +0530

    react components

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 // JSX (Transpiled Before it reaches the JS) Parcel - Babel
 
 // JSX => Babel Transpiled it to React.createElement
-const headingjsx = (<h1 className="heading" tabIndex="1">
+const headingjsx = (
+  <h1 className="heading" tabIndex="1">
     React App
-    </h1>)
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(headingjsx);
+
+// root.render(headingjsx);
+
+// React Component
+// Class Based Component - OLD
+// Function Based Component - NEW (Just normal javascript functions)
+
+const TitleComponent = () => (
+  <div>
+    <h1 className="heading">Heading</h1>
+  </div>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    <TitleComponent />
+    <h1 className="heading">React App</h1>
+  </div>
+);
+
+root.render(<HeadingComponent />);
