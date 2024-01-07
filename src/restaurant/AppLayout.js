c2882520bbf108b49a7785988e7f0fb5e57e2c84
Ashish <dev.ashishkumar0512@gmail.com>
2024-01-07 11:59:18 +0530

    use lazy loading and bundling in app

--- src/restaurant/AppLayout.js
+++ src/restaurant/AppLayout.js
@@ -1,47 +1,79 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import Header from './components/Header';
-import BodyComponent from "./components/Body";
-import {createBrowserRouter, Outlet, RouterProvider} from 'react-router-dom';
-import AboutComponent from "./components/About";
-import ContactComponent from "./components/Contact";
-import ErrorComponent from "./components/Error";
-import RestaurantMenusComponent from "./components/RestaurantMenus";
+import Header from "./components/Header";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+// eager loading
+// import AboutComponent from "./components/About";
+// import ContactComponent from "./components/Contact";
+// import ErrorComponent from "./components/Error";
+// import RestaurantMenusComponent from "./components/RestaurantMenus";
+// import BodyComponent from "./components/Body";
+
+// lazy loading
+// chunking
+// code splitting
+// dynamic bundling
+// on demand loading
+// dynamic import
+const RestaurantMenusComponent = lazy(() =>
+  import("./components/RestaurantMenus")
+);
+const AboutComponent = lazy(() => import("./components/About"));
+const ErrorComponent = lazy(() => import("./components/Error"));
+const ContactComponent = lazy(() => import("./components/Contact"));
+const BodyComponent = lazy(() => import("./components/Body"));
 
 const AppLayoutComponent = () => {
-    return (
-        <div className="app">
-            <Header />
-            {/* <BodyComponent /> */}
-            <Outlet />
-        </div>
-    );
+  return (
+    <div className="app">
+      <Header />
+      {/* <BodyComponent /> */}
+      <Outlet />
+    </div>
+  );
 };
 
 const appRouter = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <AppLayoutComponent />,
+    children: [
+      {
         path: "/",
-        element: <AppLayoutComponent/>,
-        children: [
-            {
-                path: "/",
-                element: <BodyComponent />
-            },
-            {
-                path: "about",
-                element: <AboutComponent />
-            },
-            {
-                path: "contact",
-                element: <ContactComponent />
-            },
-            {
-                path: "/restaurants/:resId",
-                element: <RestaurantMenusComponent />
-            }
-        ],
-        errorElement: <ErrorComponent />
-    }
+        element: (
+          <Suspense fallback="Loading...">
+            <BodyComponent />
+          </Suspense>
+        ),
+      },
+      {
+        path: "about",
+        element: (
+          <Suspense fallback="Loading...">
+            <AboutComponent />
+          </Suspense>
+        ),
+      },
+      {
+        path: "contact",
+        element: (
+          <Suspense fallback="Loading...">
+            <ContactComponent />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurants/:resId",
+        element: (
+          <Suspense fallback="Loading...">
+            <RestaurantMenusComponent />
+          </Suspense>
+        ),
+      },
+    ],
+    errorElement: <ErrorComponent />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
